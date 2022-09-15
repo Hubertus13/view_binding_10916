0d10f9f35cc98cdb21d69d32e5a3c5608a79f889
Hubertus13 <jubertusnova@gmail.com>
2022-09-15 08:42:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10916/Task.kt
@@ -0,0 +1,5 @@
+package com.example.view_binding_10916
+
+import androidx.appcompat.widget.DialogTitle
+
+data class Task (val chapter:String, val title:String, val details:String)
---
+++ app/src/main/java/com/example/view_binding_10916/TaskList.kt
@@ -0,0 +1,12 @@
+package com.example.view_binding_10916
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangu Tidur",
+        "Kebiasaan sehari-hari untuk membuat hari makin Indah"),
+        Task("Chapter 2","Nguli Genshin Impact","Mencari Primo buat Gacha mama Yaemiko tercinta / Keqing Wangi"),
+        Task("Chapter 3","Ngeteh","Jangan Lupa Ngetek, Ngeteh ...!!"),
+        Task("Chapter 4","dimana mama Ei","Buat Adu Mekanik Showcase Ayaka pyro"),
+        Task("Chapter 5","Upload ke Tikto","Buat Pamer ke view Showcase Ayaka Pyro C6 + Bennert C6")
+    )
+}
